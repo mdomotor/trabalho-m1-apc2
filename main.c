0e3b4bf8a957c1892dc34ed48332d1f57f89b1e6
mdomotor <mararegina.sp@gmail.com>
2015-08-29 14:51:25 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/***********************************************************
+* Nome Aluno: Lucas da Silva Marques	RA: 40795448805 *
+* Nome Aluno: Mara Regina Domotor		RA: 42566937850 *
+* Data: 29/08/2015 *
+* Descrição do programa: Trabalho M1 - Sistema de Cinema *
+************************************************************/
+
+enum statusFilme {breve, exibicao, fora_exibicao};
+
+struct dadosSessao {
+	int sala;
+	int qtdLugares;
+	float preco;
+	char filme[50];
+	char genero[30];
+	char sinopse[500];
+	enum statusFilme status;
+	char ano[4];
+};
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
